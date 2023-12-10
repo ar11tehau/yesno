7c30ae4ffb44ba45b325093dfdda0c88c951d90b
Arii D <108074652+ar11tehau@users.noreply.github.com>
2023-12-10 16:55:57 +0100

    simplified the path to the static files

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
+'use strict'
 import express from 'express'
 import path from 'path'
 import {dirname} from 'path'
 import {fileURLToPath} from 'url'
 
+const PORT = 3200
+
 const app = express()
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.use('/gif/no', express.static(path.join(__dirname, 'gif', 'no')));
-app.use('/gif/yes', express.static(path.join(__dirname, 'gif', 'yes')));
-
-app.get('/foo', function (request, response) {
-  response.send('Hello Foo!')
-})
-
-app.get('/bar', function (request, response) {
-  response.send('Hello Bar!!')
-})
-
+app.use('/gif/no', express.static('gif/no'));
+app.use('/gif/yes', express.static('gif/no'));
 const randomIndex = () => { return Math.floor(Math.random()*2) }
 const randomIndex2 = () => { return Math.floor(Math.random()*3) }
 
@@ -25,7 +19,7 @@ const get_yes_no = () => { return ['yes', 'no'][randomIndex()] }
 
 const get_random_gif = () => { return  randomIndex2() + '.gif' }
 
-app.get('/yesno', (request, response) => {
+app.get('', (request, response) => {
     let yes_no = get_yes_no()
     let alea = get_random_gif()
     response.send(`
@@ -33,13 +27,14 @@ app.get('/yesno', (request, response) => {
     <p>${yes_no}</p>
     <style>
     body{margin:0; display: flex; justify-content: center; align-items: center; background-color: black;} 
-    img {height: calc(100vh - 20px); 
+    img {width: calc(100vmin - 20px); 
     border: solid white 10px; margin: 0;}  
-    p {position: fixed; color: white; font-size: 10em;}
+    p {position: fixed; text-align: center; color: white; font-size: 10em;}
+
     </style>`)
   }
   )
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000")
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
 })
