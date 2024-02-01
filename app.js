db30af8432295de6d53bdc3cefe12413b68c6db9
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-02-01 16:47:12 +0100

    add change button

--- app.js
+++ app.js
@@ -19,14 +19,28 @@ app.get('', (request, response) => {
       const yes_no = get_yes_no()
       const alea = get_random_gif()
       response.send(`
-        <img src="/gif/${yes_no}/${alea}"></img>
-        <p>${yes_no}</p>
+        <div id="container">
+          <button onClick="changeAndRefresh()">Change</button>
+          <div id="imageDiv">
+            <img src="/gif/${yes_no}/${alea}"></img>
+            <p>${yes_no}</p>
+          </div>
+        </div>
         <style>
           body{margin:0; display: flex; justify-content: center; align-items: center; background-color: black;} 
           img {width: calc(100vmin - 20px); 
           border: solid white 10px; margin: 0;}  
           p {position: fixed; text-align: center; color: white; font-size: 10em;}
+          #container {display: flex; flex-direction: column; align-items: center;}
+          button { width: fit-content; margin: 32px; font-size: 32px;}
+          #imageDiv {display: flex; flex-direction: column; align-items: center;}
         </style>
+        <script>
+        function changeAndRefresh() {
+            // Refresh the page
+            location.reload();
+        }
+        </script>
       `)
     }
   )
